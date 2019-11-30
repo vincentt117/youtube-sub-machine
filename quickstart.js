88fb73360902582473ba2a82e93699276f68b4d7
Vincent Teng <vincentt117@gmail.com>
2019-11-29 21:46:43 -0500

    Completed API call steps

--- quickstart.js
+++ quickstart.js
@@ -4,7 +4,9 @@
 
 var fs = require('fs');
 var readline = require('readline');
-var {google} = require('googleapis');
+var {
+  google
+} = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 var SERVICE = google.youtube('v3');
 
@@ -12,13 +14,14 @@ var SERVICE = google.youtube('v3');
 // at ~/.credentials/youtube-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
+  process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'youtube-bot.json';
 
-var MAX_NUM_CREATORS = 10;
-var MAX_NUM_VID_PER_CREATOR = 2;
+var MAX_NUM_CREATORS = 5;
+var MAX_NUM_VID_PER_CREATOR = 3;
 
 var SUB_RES = {};
+var SUB_RES_FILE = "subscription_result.json";
 
 // Load client secrets from a local file.
 fs.readFile('./.credentials/client_secret.json', function processClientSecrets(err, content) {
@@ -27,7 +30,15 @@ fs.readFile('./.credentials/client_secret.json', function processClientSecrets(e
     return;
   }
   // Authorize a client with the loaded credentials, then call the YouTube API.
-  authorize(JSON.parse(content), getSubscribedTo);
+  authorize(JSON.parse(content), getSubscribedTo).then((res) => {
+    var dictstring = JSON.stringify(SUB_RES, null, "\t");
+    fs.writeFile(SUB_RES_FILE, dictstring, (error) => 
+    { 
+      if (error){
+        console.log(error);
+      }
+    });
+  });
 });
 
 /**
@@ -38,20 +49,26 @@ fs.readFile('./.credentials/client_secret.json', function processClientSecrets(e
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+  return new Promise(function (resolve, reject) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+      if (err) {
+        getNewToken(oauth2Client, callback).then((res) => {
+          resolve(res);
+        });
+      } else {
+        oauth2Client.credentials = JSON.parse(token);
+        callback(oauth2Client).then((res) => {
+          resolve(res);
+        });
+      }
+    });
+  })
 }
 
 /**
@@ -63,27 +80,31 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+  return new Promise(function (resolve, reject) {
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
     });
-  });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+      rl.close();
+      oauth2Client.getToken(code, function (err, token) {
+        if (err) {
+          console.log('Error while trying to retrieve access token', err);
+          resolve(1);
+        }
+        oauth2Client.credentials = token;
+        storeToken(token);
+        callback(oauth2Client).then((res) => {
+          resolve(res);
+        });
+      });
+    });
+  })
 }
 
 /**
@@ -115,7 +136,7 @@ function getChannel() {
     auth: auth,
     part: 'snippet,contentDetails,statistics',
     forUsername: 'GoogleDevelopers'
-  }, function(err, response) {
+  }, function (err, response) {
     if (err) {
       console.log('The API returned an error: ' + err);
       return;
@@ -125,10 +146,10 @@ function getChannel() {
       console.log('No channel found.');
     } else {
       console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-                  'it has %s views.',
-                  channels[0].id,
-                  channels[0].snippet.title,
-                  channels[0].statistics.viewCount);
+        'it has %s views.',
+        channels[0].id,
+        channels[0].snippet.title,
+        channels[0].statistics.viewCount);
     }
   });
 }
@@ -136,13 +157,14 @@ function getChannel() {
 // TODO make next three function return a promise
 
 function getSubscribedTo(auth) {
+  return new Promise(function (resolve, reject) {
     SERVICE.subscriptions.list({
       auth: auth,
       part: 'snippet',
       maxResults: MAX_NUM_CREATORS,
       mine: true,
       order: "relevance"
-    }, function(err, response) {
+    }, function (err, response) {
       if (err) {
         console.log('The API returned an error: ' + err);
         return;
@@ -151,74 +173,84 @@ function getSubscribedTo(auth) {
       if (channels.length == 0) {
         console.log('No channel found.');
       } else {
-        var promise_collection = channels.map(channel => {
-          Promise.resolve(getUploadIdOfChannel(auth, channel.snippet))
+        var promise_collection = channels.map(channel => getUploadIdOfChannel(auth, channel.snippet));
+        Promise.all(promise_collection).then((res) => {
+          resolve(res);
         });
-        Promise.all(promise_collection).then();
       }
     });
+  })
 }
 
 function getUploadIdOfChannel(auth, channelSnippet) {
-  SERVICE.channels.list({
-    auth: auth,
-    id: channelSnippet.resourceId.channelId,
-    part: 'contentDetails'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var channelPlaylistInfo = response.data.items;
-    if (channelPlaylistInfo.length >= 0) {
-      return Promise.resolve(getUploadList(auth, channelPlaylistInfo[0].contentDetails.relatedPlaylists.uploads, channelSnippet));
-    }
-  });
+  return new Promise(function (resolve, reject) {
+    SERVICE.channels.list({
+      auth: auth,
+      id: channelSnippet.resourceId.channelId,
+      part: 'contentDetails'
+    }, function (err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      var channelPlaylistInfo = response.data.items;
+      if (channelPlaylistInfo.length <= 0) {
+        resolve([]);
+      } else {
+        getUploadList(auth, channelPlaylistInfo[0].contentDetails.relatedPlaylists.uploads, channelSnippet).then((res) => {
+          resolve(res);
+        });
+      }
+    });
+  })
 }
 
 function getUploadList(auth, uploadPlaylistId, channelSnippet) {
-  SERVICE.playlistItems.list({
-    auth: auth,
-    playlistId: uploadPlaylistId,
-    part: 'snippet',
-    maxResults: MAX_NUM_VID_PER_CREATOR
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var uploads = response.data.items;
-    if (uploads.length > 0) {
-      SUB_RES[channelSnippet.title] = [];
-      var promise_collection = uploads.map(upload => getVideoDetails(auth, upload.snippet));
-      Promise.all(promise_collection).then((videoInfo) => {
-        SUB_RES[channelSnippet.title].push(videoInfo);
-        console.log(SUB_RES[channelSnippet.title]);
-    })
-  }
-  }
-      )}
-
-function getVideoDetails(auth, uploadsSnippet) {
-  return new Promise(function(resolve, reject) {
-        SERVICE.videos.list({
-          auth: auth,
-          id: uploadsSnippet.resourceId.videoId,
-          part: 'snippet'
-        }, function(err, response) {
-          var videoInfo = {};
-          if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-          }
-          var video = response.data.items;
-
-          if (video.length > 0) {
-            videoInfo['title'] = uploadsSnippet.title;
-            videoInfo['link'] = 'https://www.youtube.com/watch?v=' + uploadsSnippet.resourceId.videoId;
-            videoInfo['upload_at'] = video[0].snippet.publishedAt;
-          }
-          resolve(videoInfo);
+  return new Promise(function (resolve, reject) {
+    SERVICE.playlistItems.list({
+      auth: auth,
+      playlistId: uploadPlaylistId,
+      part: 'snippet',
+      maxResults: MAX_NUM_VID_PER_CREATOR
+    }, function (err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      var uploads = response.data.items;
+      if (uploads.length > 0) {
+        var promise_collection = uploads.map(upload => getVideoDetails(auth, upload.snippet, channelSnippet));
+        Promise.all(promise_collection).then(videoInfo => {
+          SUB_RES[channelSnippet.title] = videoInfo;
+          resolve(1);
         });
-    }) 
+      } else {
+        resolve({});
+      }
+    })
+  })
+}
+
+function getVideoDetails(auth, uploadsSnippet, channelSnippet) {
+  return new Promise(function (resolve, reject) {
+    SERVICE.videos.list({
+      auth: auth,
+      id: uploadsSnippet.resourceId.videoId,
+      part: 'snippet'
+    }, function (err, response) {
+      var videoInfo = {};
+
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        resolve(videoInfo);
+      }
+      var video = response.data.items;
+      if (video.length > 0) {
+        videoInfo['title'] = uploadsSnippet.title;
+        videoInfo['link'] = 'https://www.youtube.com/watch?v=' + uploadsSnippet.resourceId.videoId;
+        videoInfo['upload_at'] = video[0].snippet.publishedAt;
+      }
+      resolve(videoInfo);
+    });
+  })
 }
